bc518a81c43da5cc6517bc2cf45ad4d6d50e25cc
fisker <lionkay@gmail.com>
2026-01-04 22:11:30 +0800

    Enable coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vitest/config";
 
-export default {
+export default defineConfig({
   test: {
     include: ["tests/**/format.test.js"],
     setupFiles: ["./tests/setup.js"],
     snapshotSerializers: ["jest-snapshot-serializer-raw"],
+    coverage: {
+      enabled: true,
+      reporter: ["lcov", "text"],
+    },
   },
-};
+});
